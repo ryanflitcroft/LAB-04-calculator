0a001aa489a7808104612f81b43d06fee264f333
ryan flitcroft <ryanflitcroft@ymail.com>
2021-10-07 14:37:16 -0700

    add/sub/mult/div calculations and failing tests, with input/buttons for all in html

--- app.js
+++ app.js
@@ -1,11 +1,75 @@
 // import functions
-import { add } from './calculations.js';
+import { add, subtract, multiply, divide } from './calculations.js';
 
+// console.log(add(50, 212));
 console.log(add(50, 212));
+
 // reference needed DOM elements
-
-
+const addNumber1 = document.getElementById('add-number-1');
+const addNumber2 = document.getElementById('add-number-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
+console.log(addNumber1, addNumber2, addButton, addAnswer);
 // set event listeners 
+addButton.addEventListener('click', () => {
+
     // get user input(s)
+    const num1 = +addNumber1.value;
+    const num2 = +addNumber2.value;
+
     // do any needed work with the value(s)
+    const sum = add(num1, num2);
+
     // update DOM to reflect new value(s)
+    addAnswer.textContent = sum;    
+});
+
+
+console.log(subtract(50, 40));
+
+const subtractNumber1 = document.getElementById('subtract-number-1');
+const subtractNumber2 = document.getElementById('subtract-number-2');
+const subtractButton = document.getElementById('subtract-button');
+const subtractAnswer = document.getElementById('subtract-answer');
+
+console.log(subtractNumber1, subtractNumber2, subtractButton, subtractAnswer);
+
+subtractButton.addEventListener('click', () => {
+    const num1 = +subtractNumber1.value;
+    const num2 = +subtractNumber2.value;
+    const diff = subtract(num1, num2);
+
+    subtractAnswer.textContent = diff;
+});
+
+
+const multiplyNumber1 = document.getElementById('multiply-number-1');
+const multiplyNumber2 = document.getElementById('multiply-number-2');
+const multiplyButton = document.getElementById('multiply-button');
+const multiplyAnswer = document.getElementById('multiply-answer');
+
+console.log(multiplyNumber1, multiplyNumber2, multiplyButton, multiplyAnswer);
+
+multiplyButton.addEventListener('click', () => {
+    const num1 = +multiplyNumber1.value;
+    const num2 = +multiplyNumber2.value;
+    const product = multiply(num1, num2);
+
+    multiplyAnswer.textContent = product;
+});
+
+
+const divideNumber1 = document.getElementById('divide-number-1');
+const divideNumber2 = document.getElementById('divide-number-2');
+const divideButton = document.getElementById('divide-button');
+const divideAnswer = document.getElementById('divide-answer');
+
+console.log(divideNumber1, divideNumber2, divideButton, divideAnswer);
+
+divideButton.addEventListener('click', () => {
+    const num1 = +divideNumber1.value;
+    const num2 = +divideNumber2.value;
+    const product = divide(num1, num2);
+
+    divideAnswer.textContent = product;
+});
--- test/tests.js
+++ test/tests.js
@@ -1,24 +1,66 @@
 // IMPORT MODULES under test here:
 //import { add } from '../calculator.js';
 
-import { add } from '../calculations.js';
-
+import { add, subtract, multiply, divide } from '../calculations.js';
 const test = QUnit.test;
 
-
+//ADDITION TEST
 // name your test by what it is testing
 test('adds two numbers 5 and 3', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const x = 5;
     const y = 3;
-    const expected = 8;
-    
+    //const expected = 8;
+    const expected = 9;
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = add(x, y); // use your function here
+    // use your function here
+    const actual = add(x, y);
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+console.log(add(3, 10));
+
+//SUBTRACTION TEST
+test('subtracts two numbers 5 and 3', (expect) => {
+    const x = 5;
+    const y = 3;
+    //const expected = 2;
+    const expected = 33;
+
+    const actual = subtract(x, y);
+    expect.equal(actual, expected);
+});
+
+console.log(subtract(10, 6));
+
+
+//MULTIPLY TEST
+test('multiply two numbers 5 and 3', (expect) => {
+    const x = 5;
+    const y = 3;
+    //const expected = 15;
+    const expected = 11;
+
+    const actual = multiply(x, y);
+    expect.equal(actual, expected);
+});
+console.log(multiply(5, 10));
+
+
+//DIVIDE TEST
+test('divide two numbers 6 and 3', (expect) => {
+    const x = 6;
+    const y = 3;
+    //const expected = 2;
+    const expected = 22;
+
+    const actual = divide(x, y);
+    expect.equal(actual, expected);
+});
+
+console.log(divide(10, 2));
